09cc73970b0580d3ac3a800d1ab069ccc124a6c8
Lucas-haddy <lucas-hadfield@live.nmit.ac.nz>
2026-09-21 15:46:21 +1200

    created a silly little trivia bot which spouts nonsense XD

---
+++ JavaScript/Variables & Strings/triviaBot.js
@@ -0,0 +1,17 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+const botName = "Bats";
+const botLocation = "Gotham";
+const favoriteLanguage = "Justice";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " is best served lukewarm.";
+console.log(codingFact);
+codingFact = favoriteLanguage + " is also very tiring.";
+console.log(codingFact);
+codingFact = "I have bee awake for 6 days because of " + favoriteLanguage;
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation + ".");
