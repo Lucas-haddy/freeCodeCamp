3c1f16b3a91223d9bec6c68839ba53c9169e2747
Lucas-haddy <lucas-hadfield@live.nmit.ac.nz>
2026-09-22 08:45:01 +1200

    created a simple sentence maker

---
+++ JavaScript/Variables & Strings/sentenceMaker.js
@@ -0,0 +1,19 @@
+let adjective = "ancient";
+let noun = "dragon";
+let verb = "angry";
+let place = "cavernous mountain peak";
+let adjective2 = "enormous";
+let noun2 = "roasted marshmellows";
+
+const firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`
+console.log(`First story: ${firstStory}`);
+
+adjective = "fluffy";
+noun = "red panda";
+verb = "sneezing";
+place = "cozy hollow tree";
+adjective2 = "twitchy";
+noun2 = "cinnamon rolls";
+
+const secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+console.log(`Second story: ${secondStory}`);
