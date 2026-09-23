abdc8e877ece04c4123b6150a563c61386113c4b
Lucas-haddy <lucas-hadfield@live.nmit.ac.nz>
2026-09-24 10:09:49 +1200

    changed name of previous insert and added new FCC workshop for repeat() and replace()

--- JavaScript/Variables & Strings/stringTransformer.js
+++ JavaScript/Variables & Strings/stringTransformer.js
@@ -1,28 +1,25 @@
-const userInput = "   Hello World!   ";
-console.log("Original input:");
-console.log(userInput);
+const originalString = "I love cats.";
+console.log("Original string:");
+console.log(originalString);
 
-const cleanedInput = userInput.trim();
-console.log("Result of trimming whitespace from both ends:");
-console.log(cleanedInput);
+const replacedString = originalString.replace("cats", "dogs");
+console.log("After using the replace() method:");
+console.log(replacedString);
 
-const trimmedStart = userInput.trimStart();
-console.log("After using the trimStart() method, leading spaces removed:");
-console.log(trimmedStart);
+const exampleSentence = "I love cats and cats are so much fun!";
+console.log("Original sentence:");
+console.log(exampleSentence);
 
-const trimmedEnd = userInput.trimEnd();
-console.log("After using the trimEnd() method, trailing spaces removed:");
-console.log(trimmedEnd);
+const dogsOnlySentence = exampleSentence.replaceAll("cats", "dogs");
+console.log("Replacing all occurrences of cats with dogs:");
+console.log(dogsOnlySentence);
 
-const upperCaseInput = cleanedInput.toUpperCase();
-console.log("Result of using the toUpperCase() method:");
-console.log(upperCaseInput);
+const learningSentence = "I love learning!";
+console.log("Original learning sentence:");
+console.log(learningSentence);
 
-const lowerCaseInput = cleanedInput.toLowerCase();
-console.log("Result of using the toLowerCase() method:");
-console.log(lowerCaseInput);
+const repeatedLove = "love ".repeat(3).trimEnd();
+console.log(repeatedLove);
 
-const lowercaseWord = "camelcase";
-const camelCasedVersion = lowercaseWord.slice(0, 5) + lowercaseWord[5].toUpperCase() + lowercaseWord.slice(-3);
-console.log("Camel cased version:");
-console.log(camelCasedVersion);
+const newSentence = `I ${repeatedLove} learning.`;
+console.log(newSentence);
